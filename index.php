fdb5c04c6487d442973cc26cd3b19fd63322ecb1
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-01-08 14:44:01 -0300

    Dependencias do bulma instaladas

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TGT - Together</title>
+
+    <!-- ICONS OF BULMA -->
+    <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
+
+    <!-- STYLES OF BULMA -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+</head>
+<body>
+    
+</body>
+</html>
